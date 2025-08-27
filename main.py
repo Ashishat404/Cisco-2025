1ecde364b29558e75724a1a426b76a4ad2fc50f0
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-08-27 08:59:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ import matplotlib.pyplot as plt
 def parse_configs(config_dir):
     configs = {}
     for root, _, files in os.walk(config_dir):
+        for file in files:
+            if file.endswith(".dump"):
